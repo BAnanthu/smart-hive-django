130fd6a85b182d73cad64d0b63208722c5a56094
= <=>
2021-09-21 21:42:25 +0530

    api

--- smarthive/settings.py
+++ smarthive/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%74my3d_-45pb%1u7yeh=d++4w_hlyqrus0ogk$a_qiek7=vdr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['smart-hive-django.herokuapp.com']
+ALLOWED_HOSTS = ['smart-hive-django.herokuapp.com','127.0.0.1']
 
 
 # Application definition
