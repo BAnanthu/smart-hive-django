0fe9a887af20a407e00bbd19d12aa41d832f9143
= <=>
2021-10-09 20:42:42 +0530

    prrogres select donbe

--- identify/urls.py
+++ identify/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('identify/<int:category_id>/<int:subcategory_id>/<int:level_id>', views.Identify.as_view(),name='IDENTIFY (ID)'),
+    path('identify/', views.Identify.as_view(),name='IDENTIFY (ID)'),
     path('protect/<int:category_id>/<int:subcategory_id>/<int:level_id>', views.Protect.as_view(),name='PROTECT (PR)'),
     path('detect/<int:category_id>/<int:subcategory_id>/<int:level_id>', views.Detect.as_view(),name='DETECT (DE)'),
     path('respond/<int:category_id>/<int:subcategory_id>/<int:level_id>', views.Respond.as_view(),name='RESPOND (RS)'),
--- identify/views.py
+++ identify/views.py
@@ -9,44 +9,44 @@ from api.models import Function, Category, SubCategory, Level, Option
 class Identify(View):
 
     @staticmethod
-    def get(request, category_id, subcategory_id, level_id):
-        if category_id == 0 or subcategory_id == 0 or level_id == 0:
-            print("ddd")
-            category_id = 1
-            subcategory_id = 1
-            level_id = 1
-        function = Function.objects.all()
-        category = Category.objects.filter(function_id=1)
-        sub_category = SubCategory.objects.get(id=subcategory_id, category_id=category_id)
-        level = Level.objects.get(id=level_id)
-        options = Option.objects.filter(level_id=level_id)
-        if Level.objects.filter(id=level_id + 1).exists():
-            nextitem = {'next_sub': subcategory_id, 'next_level': level_id + 1}
-            # preitem = {'pre_sub': subcategory_id, 'pre_level': level_id - 1}
-        else:
-            print(sub_category)
-            nextitem = {'next_sub': subcategory_id + 1, 'next_level': 1}
-            # preitem = {'pre_sub': subcategory_id - 1, 'pre_level': 1}
+    def get(request, category_id=None, subcategory_id=None, level_id=None):
+        # if category_id == 0 or subcategory_id == 0 or level_id == 0:
+        #     print("ddd")
+        #     category_id = 1
+        #     subcategory_id = 1
+        #     level_id = 1
+        # function = Function.objects.all()
+        # category = Category.objects.filter(function_id=1)
+        # sub_category = SubCategory.objects.get(id=subcategory_id, category_id=category_id)
+        # level = Level.objects.get(id=level_id)
+        # options = Option.objects.filter(level_id=level_id)
+        # if Level.objects.filter(id=level_id + 1).exists():
+        #     nextitem = {'next_sub': subcategory_id, 'next_level': level_id + 1}
+        #     # preitem = {'pre_sub': subcategory_id, 'pre_level': level_id - 1}
+        # else:
+        #     print(sub_category)
+        #     nextitem = {'next_sub': subcategory_id + 1, 'next_level': 1}
+        #     # preitem = {'pre_sub': subcategory_id - 1, 'pre_level': 1}
+        #
+        # if Level.objects.filter(id=level_id - 1).exists():
+        #     preitem = {'pre_sub': subcategory_id, 'pre_level': level_id - 1}
+        # else:
+        #     latestid = Level.objects.latest('id').id
+        #     preitem = {'pre_sub': subcategory_id - 1, 'pre_level': latestid}
+        #
+        # context = {'function': function,
+        #            'active': 1,
+        #            'progressbars': category,
+        #            'subcat': sub_category,
+        #            'question': sub_category.subcategory_details,
+        #            'level': level,
+        #            'options': options,
+        #            'next': nextitem,
+        #            'pre': preitem,
+        #             'funname':'IDENTIFY (ID)'
+        #            }
 
-        if Level.objects.filter(id=level_id - 1).exists():
-            preitem = {'pre_sub': subcategory_id, 'pre_level': level_id - 1}
-        else:
-            latestid = Level.objects.latest('id').id
-            preitem = {'pre_sub': subcategory_id - 1, 'pre_level': latestid}
-
-        context = {'function': function,
-                   'active': 1,
-                   'progressbars': category,
-                   'subcat': sub_category,
-                   'question': sub_category.subcategory_details,
-                   'level': level,
-                   'options': options,
-                   'next': nextitem,
-                   'pre': preitem,
-                    'funname':'IDENTIFY (ID)'
-                   }
-
-        return render(request, 'identify.html', context)
+        return render(request, 'identify.html')
 
 
 class Protect(View):
